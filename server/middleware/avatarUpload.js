27ed5d222095f95445f56fa7c5cfbfc2971a4b10
avronilrajib <avronilrajib@gmail.com>
2024-07-25 11:31:23 +0600

    avatar upload

--- server/middleware/avatarUpload.js
+++ server/middleware/avatarUpload.js
@@ -1,31 +1,15 @@
 const multer=require('multer');
 const path=require('path');
 
-const UPLOAD_LOCATOIN='./uploads/';
-const storage=multer.diskStorage({
-    destination:(req,file,cb)=>{
-       cb(null,UPLOAD_LOCATOIN) 
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, './uploads/');
     },
-    filename:(req,file,cb)=>{
+    filename: function (req, file, cb) {
         const fileExt=path.extname(file.originalname);
         const fileName=file.originalname.replace(fileExt,"").toLowerCase().split(" ").join("_")+"_"+Date.now();
-        console.log(fileName);
         cb(null,fileName+fileExt)
-
     }
-})
-var upload=multer({
-    storage:storage,
-    limits:{
-        fileSize:1000000,
-    },
-    fileFilter:(req,file,cb)=>{
-        if(file.mimetype==='image/png' || file.mimetype==='image/jpg'||file.mimetype==='image/jpeg'){
-            cb(null,true);
-        }else{
-            cb(null,false);
-        }
-    }
-});
-
+  })
+  const upload = multer({ storage: storage });
 module.exports=upload;
